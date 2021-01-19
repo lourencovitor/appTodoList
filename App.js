a4600b40d2b9245696c38ff66361eee27d6e3535
lourencovitor <vitor.brother17@outlook.com.br>
2021-01-19 00:28:49 -0300

    finalize application todo-list

--- App.js
+++ App.js
@@ -1,25 +1,106 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 import {
   StyleSheet,
   Text,
   View,
   TextInput,
   TouchableOpacity,
+  FlatList,
+  KeyboardAvoidingView,
+  Platform,
+  Keyboard,
+  Alert,
+  AsyncStorage,
 } from "react-native";
 
 export default function App() {
+  const [task, setTask] = useState([]);
+  const [newTask, setNewTask] = useState("");
+  async function addTask() {
+    const search = task.filter((task) => task === newTask);
+
+    if (search.length !== 0) {
+      Alert.alert("Atenção", "Nome da tarefa repetido!");
+      return;
+    }
+
+    setTask([...task, newTask]);
+    setNewTask("");
+
+    Keyboard.dismiss();
+  }
+
+  async function removeTask(item) {
+    Alert.alert(
+      "Deletar Task",
+      "Tem certeza que deseja remover está task?",
+      [
+        {
+          text: "Cancel",
+          onPress: () => {
+            return;
+          },
+          style: "cancel",
+        },
+        {
+          text: "Ok",
+          onPress: () => setTask(task.filter((tasks) => tasks !== item)),
+        },
+      ],
+      { cancelable: false }
+    );
+  }
+
+  useEffect(() => {
+    setTask([]);
+  }, []);
+
   return (
     <>
-      <View style={styles.container}>
-        <View style={styles.Body}>
-          <Text>Hello World!</Text>
+      <KeyboardAvoidingView
+        keyboardVerticalOffset={0}
+        behavior="padding"
+        style={{ flex: 1 }}
+        enabled={Platform.OS === "ios"}
+      >
+        <View style={styles.container}>
+          <View style={styles.Body}>
+            <FlatList
+              style={styles.FlatList}
+              data={task}
+              keyExtractor={(item) => item.toString()}
+              showsVerticalScrollIndicator={false}
+              renderItem={({ item }) => (
+                <View style={styles.ContainerView}>
+                  <Text style={styles.Texto}>{item}</Text>
+                  <TouchableOpacity onPress={() => removeTask(item)}>
+                    <MaterialIcons
+                      name="delete-forever"
+                      size={25}
+                      color="#f64c75"
+                    />
+                  </TouchableOpacity>
+                </View>
+              )}
+            />
+          </View>
+          <View style={styles.Form}>
+            <TextInput
+              style={styles.Input}
+              placeholderTextColor="#999"
+              autoCorrect={true}
+              placeholder="Adicione uma tarefa"
+              maxLength={25}
+              onChangeText={(text) => setNewTask(text)}
+              value={newTask}
+            />
+            <TouchableOpacity onPress={() => addTask()} style={styles.Button}>
+              <Ionicons name="ios-add" size={25} color="#FFF" />
+            </TouchableOpacity>
+          </View>
         </View>
-        <View>
-          <TextInput />
-          <TouchableOpacity />
-        </View>
-      </View>
+      </KeyboardAvoidingView>
     </>
   );
 }
@@ -41,5 +122,50 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignSelf: "stretch",
     flexDirection: "row",
+    padding: 13,
+    borderTopWidth: 1,
+    borderColor: "#eee",
+  },
+  Input: {
+    flex: 1,
+    height: 40,
+    backgroundColor: "#eee",
+    borderRadius: 4,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: "#eee",
+  },
+  Button: {
+    height: 40,
+    width: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1c6cce",
+    borderRadius: 4,
+    marginLeft: 10,
+  },
+  FlatList: {
+    flex: 1,
+    marginTop: 5,
+  },
+  ContainerView: {
+    marginBottom: 15,
+    padding: 15,
+    borderRadius: 4,
+    backgroundColor: "#eee",
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderRadius: 1,
+    borderColor: "#eee",
+  },
+  Texto: {
+    fontSize: 14,
+    color: "#333",
+    fontWeight: "bold",
+    marginTop: 4,
+    textAlign: "center",
   },
 });
